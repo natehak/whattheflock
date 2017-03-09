736d22a99b51c4d0e5bc573f94e6797d0625a985
Nathan Hakkakzadeh <nate.hak@gmail.com>
2017-03-08 22:02:51 -0800

    Fixed boid

--- src/boid.h
+++ src/boid.h
@@ -5,12 +5,17 @@
 class Boid {
 public:
     Vec3 pos;
-    float max_speed;
-    float speed;
-    float max_accel; // Some fraction of max_speed
-    Rotation orientation;
+    Vec3 velocity;
 
-    Boid(): pos(Vec3()), max_speed(100.0), speed(0.0), max_accel(0.25), orientation(Rotation()) { }
+    Vec3 forward;
+    Vec3 up;
+    Vec3 side;
+
+    float max_speed;
+    float max_accel; // Some fraction of max_speed
+
+    Boid(): pos(Vec3()), velocity(Vec3()), forward(Vec3(0.0, 0.0, 1.0)), up(Vec3(0.0, 1.0, 0.0)),
+        side(Vec3(1.0, 0.0, 0.0)), max_speed(100.0), max_accel(0.25) { }
 
     Boid update();
 };
